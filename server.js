f96e2481a40517bb9da01f733fef6426deb05465
jessej-samuel <1234jessej@gmail.com>
2022-10-24 22:40:20 +0530

    feat: added database connectivity with prisma

--- server.js
+++ server.js
@@ -2,41 +2,65 @@ import express from "express";
 const app = express();
 const PORT = 5000;
 
-const tasks = [
-  {
-    id: 1,
-    title: "Task One",
-    description: "Description of task one",
-  },
-  {
-    id: 2,
-    title: "Task Two",
-    description: "Description of task two",
-  },
-  {
-    id: 3,
-    title: "Task Three",
-    description: "Description of task three",
-  },
-];
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const createTask = async (title) => {
+  await prisma.task.create({
+    data: {
+      title: title,
+      created: new Date(),
+      deadline: new Date(),
+    },
+  });
+};
+
+const getTasks = async () => {
+  const tasks = await prisma.task.findMany();
+  return tasks;
+};
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/tasks", (req, res) => {
-  res.json(tasks);
+  getTasks()
+    .then((tasks) => res.json(tasks))
+    .catch((err) => console.log(err));
 });
 
 app.post("/tasks", async (req, res) => {
-  const { title, description } = req.body;
-  tasks.push({
-    id: tasks.length + 1,
-    title: title,
-    description: description,
-  });
-  res.redirect("/tasks");
+  const { title } = req.body;
+  await createTask(title)
+    .then(async () => {
+      getTasks()
+        .then((tasks) => res.json(tasks))
+        .catch((err) => console.log(err));
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
 });
+
+app.get()
 
 app.listen(PORT, () => {
   console.log("Server is running at", PORT);
